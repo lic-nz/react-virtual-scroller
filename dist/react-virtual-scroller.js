9115b829e9ea6fc5765704a9281b141ae40f56cd
Brad Christensen <brad.christensen@enlighten.co.nz>
2016-11-24 15:36:35 +1300

    Use updated version of react-load-mask and bump to 2.1.4-lic.3

--- dist/react-virtual-scroller.js
+++ dist/react-virtual-scroller.js
@@ -805,8 +805,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	  value: true
 	});
 
-	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 	var _react = __webpack_require__(2);
@@ -837,7 +835,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  function LoadMask() {
 	    _classCallCheck(this, LoadMask);
 
-	    return _possibleConstructorReturn(this, Object.getPrototypeOf(LoadMask).apply(this, arguments));
+	    return _possibleConstructorReturn(this, (LoadMask.__proto__ || Object.getPrototypeOf(LoadMask)).apply(this, arguments));
 	  }
 
 	  _createClass(LoadMask, [{
@@ -850,7 +848,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	      return _react2.default.createElement(
 	        'div',
-	        _extends({}, props, { className: className }),
+	        { style: props.style, className: className },
 	        _react2.default.createElement(_Loader2.default, { size: props.size, theme: props.theme })
 	      );
 	    }
@@ -876,8 +874,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
-
-	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -910,7 +906,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  function Loader() {
 	    _classCallCheck(this, Loader);
 
-	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Loader).apply(this, arguments));
+	    return _possibleConstructorReturn(this, (Loader.__proto__ || Object.getPrototypeOf(Loader)).apply(this, arguments));
 	  }
 
 	  _createClass(Loader, [{
@@ -927,7 +923,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	      return _react2.default.createElement(
 	        'div',
-	        _extends({}, props, { style: style, className: className }),
+	        { style: style, className: className },
 	        _react2.default.createElement('div', { className: LOADBAR_CLASSNAME + ' ' + LOADBAR_CLASSNAME + '--1' }),
 	        _react2.default.createElement('div', { className: LOADBAR_CLASSNAME + ' ' + LOADBAR_CLASSNAME + '--2' }),
 	        _react2.default.createElement('div', { className: LOADBAR_CLASSNAME + ' ' + LOADBAR_CLASSNAME + '--3' }),
